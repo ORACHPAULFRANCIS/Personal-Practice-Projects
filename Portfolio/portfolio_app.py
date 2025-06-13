914d7ae7cd72bf58fda4930da6c9eacbadd3f395
Orach Paul Francis <157469714+ORACHPAULFRANCIS@users.noreply.github.com>
2025-06-13 10:06:27 +0300

    Update portfolio_app.py

--- Portfolio/portfolio_app.py
+++ Portfolio/portfolio_app.py
@@ -86,13 +86,13 @@ with col2:
     st.markdown("#### Top Problem Solving Voice | Petroleum Engineer | Python Coder | Chairperson Membership Committee (Geological Society of Uganda) | IWCF 1 | Petrophysics | Power BI | Agile Project Management | Solutions Architecture")
     st.markdown("""
 <details class='card'>
-  <summary style='font-weight:600; font-size:18px;'>📌 About Me</summary>
+  <summary style='font-weight:600; font-size:18px;'> About Me</summary>
   <div style='padding-top:10px; font-size:16px;'>
-    Dynamic and results-driven Petroleum Engineer with strong expertise in subsurface data interpretation, formation evaluation, drilling planning, data analysis with Python, and technical project execution within the oil and gas sector.<br><br>
+    Dynamic and results-driven Petroleum Engineer with strong expertise in subsurface data interpretation, formation evaluation, drilling planning, data analysis with Python, and technical project execution within the oil and gas sector.
 
-    Demonstrated success in aligning technical solutions with field operations, enhancing reservoir performance, and supporting HSE compliance across upstream operations.<br><br>
+    Demonstrated success in aligning technical solutions with field operations, enhancing reservoir performance, and supporting HSE compliance across upstream operations.
 
-    Skilled in Geolog, Petrel, Python, Numpy, Pandas, Matplotlib, Power BI, Streamlit, Flask and other industry-standard tools to deliver data-driven insights, optimize well planning, and drive operational excellence.<br><br>
+    Skilled in Geolog, Petrel, Python, Numpy, Pandas, Matplotlib, Power BI, Streamlit, Flask and other industry-standard tools to deliver data-driven insights, optimize well planning, and drive operational excellence.
 
     And oh, I currently use Streamlit for all my frontend stuff and Python for all my backend. But yeah...I am capable of learning anything (try me 😄).
   </div>
